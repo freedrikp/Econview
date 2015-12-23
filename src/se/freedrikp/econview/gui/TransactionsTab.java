dc38fe4d313af2473cf4a7719b7be007167a7180
freedrikp <fredrik40@msn.com>
2015-12-23 21:49:27 +0100

    Created a class TransactionDialog which handles the dialog.

--- src/se/freedrikp/econview/gui/TransactionsTab.java
+++ src/se/freedrikp/econview/gui/TransactionsTab.java
@@ -73,41 +73,8 @@ public class TransactionsTab extends JPanel implements Observer {
 		btnAddTransaction.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnAddTransaction.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// String accountName = askUser("Account Name",
-				// "What is the name of the account?", null, db
-				// .getAccountNames().toArray());
-				// if (accountName == null) {
-				// return;
-				// }
-				// String transactionAmount = askUser("Transaction Amount",
-				// "What is the amount of the transaction?", null, null);
-				// if (transactionAmount == null) {
-				// return;
-				// }
-				// String transactionDate = askUser("Transaction Date",
-				// "What is the date of the transaction?", null, null);
-				// if (transactionDate == null) {
-				// return;
-				// }
-				// String transactionComment = askUser("Transaction Comment",
-				// "What is the comment of the tansaction?", null, null);
-				// if (transactionComment == null) {
-				// return;
-				// }
-				// try {
-				// db.addTransaction(accountName, Double
-				// .parseDouble(transactionAmount),
-				// new SimpleDateFormat("yyyy-mm-dd")
-				// .parse(transactionDate), transactionComment);
-				// } catch (NumberFormatException e1) {
-				// e1.printStackTrace();
-				// } catch (ParseException e1) {
-				// e1.printStackTrace();
-				// }
-
-				Object[] transactionDetails = askUserTransaction(db
-						.getAccountNames().toArray(), null, null, new Date(),
-						null);
+				Object[] transactionDetails = TransactionDialog.showAddDialog(db
+						.getAccountNames().toArray());
 
 				if (transactionDetails != null) {
 					try {
@@ -136,54 +103,8 @@ public class TransactionsTab extends JPanel implements Observer {
 		btnEditTransaction.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnEditTransaction.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// String accountName = askUser(
-				// "Account Name",
-				// "What is the name of the account?",
-				// (String) transactionsTable.getModel().getValueAt(
-				// transactionsTable.getSelectedRow(), 1),
-				// db.getAccountNames().toArray());
-				// if (accountName == null) {
-				// return;
-				// }
-				// String transactionAmount = askUser(
-				// "Transaction Amount",
-				// "What is the amount of the transaction?",
-				// (String) transactionsTable.getModel().getValueAt(
-				// transactionsTable.getSelectedRow(), 2), null);
-				// if (transactionAmount == null) {
-				// return;
-				// }
-				// String transactionDate = askUser(
-				// "Transaction Date",
-				// "What is the date of the transaction?",
-				// (String) transactionsTable.getModel().getValueAt(
-				// transactionsTable.getSelectedRow(), 3), null);
-				// if (transactionDate == null) {
-				// return;
-				// }
-				// String transactionComment = askUser(
-				// "Transaction Comment",
-				// "What is the comment of the tansaction?",
-				// (String) transactionsTable.getModel().getValueAt(
-				// transactionsTable.getSelectedRow(), 4), null);
-				// if (transactionComment == null) {
-				// return;
-				// }
-				// try {
-				// db.editTransaction(Long
-				// .parseLong((String) transactionsTable.getModel()
-				// .getValueAt(transactionsTable.getSelectedRow(),
-				// 0)), accountName, Double
-				// .parseDouble(transactionAmount),
-				// new SimpleDateFormat("yyyy-mm-dd")
-				// .parse(transactionDate), transactionComment);
-				// } catch (NumberFormatException e1) {
-				// e1.printStackTrace();
-				// } catch (ParseException e1) {
-				// e1.printStackTrace();
-				// }
 				try {
-					Object[] transactionDetails = askUserTransaction(
+					Object[] transactionDetails = TransactionDialog.showEditDialog(
 							db.getAccountNames().toArray(),
 							(String) transactionsTable.getModel().getValueAt(
 									transactionsTable.getSelectedRow(), 1),
@@ -316,131 +237,6 @@ public class TransactionsTab extends JPanel implements Observer {
 		numDeposits.setText(Long.toString(db.getNumberOfDeposits()));
 		numWithdrawals.setText(Long.toString(db.getNumberOfWithdrawals()));
 		repaint();
-	}
-
-	private Object[] askUserTransaction(final Object[] accountValues,
-			String selectedAccount, String selectedAmount, Date selectedDate,
-			String selectedComment) {
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		final JPanel multiAccountPanel = new JPanel();
-		multiAccountPanel.setLayout(new BoxLayout(multiAccountPanel,
-				BoxLayout.Y_AXIS));
-
-		final LinkedList<JComponent> multiAccounts = new LinkedList<JComponent>();
-
-		JComboBox accountField = new JComboBox(accountValues);
-		accountField.setSelectedItem(selectedAccount);
-		JPanel accountPanel = new JPanel();
-		accountPanel.add(new JLabel(Utilities
-				.getString("ADD_TRANSACTION_ACCOUNT") + ":"));
-		accountPanel.add(accountField);
-		multiAccountPanel.add(accountPanel);
-		multiAccounts.add(accountField);
-
-		JTextField amountField = new JTextField("", 7);
-		amountField.setText(selectedAmount);
-		JPanel amountPanel = new JPanel();
-		amountPanel.add(new JLabel(Utilities
-				.getString("ADD_TRANSACTION_AMOUNT") + ":"));
-		amountPanel.add(amountField);
-		multiAccountPanel.add(amountPanel);
-		multiAccounts.add(amountField);
-
-//		JScrollPane multiAccountScrollPane = new JScrollPane();
-//		multiAccountScrollPane.setViewportView(multiAccountPanel);
-//		panel.add(multiAccountScrollPane);
-		panel.add(multiAccountPanel);
-
-		JPanel multiAccountControlPanel = new JPanel();
-		JButton removeMultiAccountButton = new JButton("-");
-		JButton addMultiAccountButton = new JButton("+");
-
-		removeMultiAccountButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (multiAccounts.size() >= 4) {
-					multiAccountPanel.remove(multiAccounts.removeLast().getParent());
-					multiAccountPanel.remove(multiAccounts.removeLast().getParent());
-				}
-				multiAccountPanel.revalidate();
-			}
-		});
-
-		addMultiAccountButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JComboBox accountField = new JComboBox(accountValues);
-				accountField.setSelectedIndex(-1);
-				JPanel accountPanel = new JPanel();
-				accountPanel.add(new JLabel(Utilities
-						.getString("ADD_TRANSACTION_ACCOUNT") + ":"));
-				accountPanel.add(accountField);
-				multiAccountPanel.add(accountPanel);
-				multiAccounts.add(accountField);
-
-				JTextField amountField = new JTextField("", 7);
-				JPanel amountPanel = new JPanel();
-				amountPanel.add(new JLabel(Utilities
-						.getString("ADD_TRANSACTION_AMOUNT") + ":"));
-				amountPanel.add(amountField);
-				multiAccountPanel.add(amountPanel);
-				multiAccounts.add(amountField);
-
-				multiAccountPanel.revalidate();
-			}
-		});
-
-		multiAccountControlPanel.add(removeMultiAccountButton);
-		multiAccountControlPanel.add(addMultiAccountButton);
-		panel.add(multiAccountControlPanel);
-
-		// JTextField dateField = new JTextField("", 7);
-		// dateField.setText(selectedDate);
-		JPanel datePanel = new JPanel();
-		datePanel.add(new JLabel(Utilities.getString("ADD_TRANSACTION_DATE")
-				+ ":"));
-		// -----
-		JDateChooser dateSelector = new JDateChooser(selectedDate,
-				dateFormat.toPattern());
-		datePanel.add(dateSelector);
-		// -----
-		// datePanel.add(dateField);
-		panel.add(datePanel);
-
-		JTextField commentField = new JTextField("", 15);
-		commentField.setText(selectedComment);
-		JPanel commentPanel = new JPanel();
-		commentPanel.add(new JLabel(Utilities
-				.getString("ADD_TRANSACTION_COMMENT") + ":"));
-		commentPanel.add(commentField);
-		panel.add(commentPanel);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportView(panel);
-		scrollPane.setPreferredSize(new Dimension(Integer.parseInt(Utilities.getConfig("ADD_TRANSACTION_PANEL_HEIGHT")),Integer.parseInt(Utilities.getConfig("ADD_TRANSACTION_PANEL_WIDTH"))));
-
-		int result = JOptionPane.showConfirmDialog(null, scrollPane,
-				Utilities.getString("TRANSACTION_DETAILS"),
-				JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
-				null);
-
-		if (result == JOptionPane.OK_OPTION) {
-			Object[] details = new Object[multiAccounts.size()+2];
-			int i = 0;
-			for(JComponent c: multiAccounts){
-				if (i % 2 == 0){
-					details[i]=((JComboBox)c).getSelectedItem();
-				}else{
-					details[i]=((JTextField)c).getText();
-				}
-				i++;
-			}
-//			details[0] = accountField.getSelectedItem();
-//			details[1] = amountField.getText();
-			details[i] = dateSelector.getDate();// dateField.getText();
-			details[i+1] = commentField.getText();
-			return details;
-		}
-		return null;
 	}
 
 	private void updateTransactionList() {
