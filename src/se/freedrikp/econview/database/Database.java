572bd5dd97103dbcf8aac10ff7f8ddd981073e81
freedrikp <fredrik40@msn.com>
2016-01-18 21:42:35 +0100

    Improved sorting of select between dates.

--- src/se/freedrikp/econview/database/Database.java
+++ src/se/freedrikp/econview/database/Database.java
@@ -582,7 +582,7 @@ public class Database extends Observable {
 						+ sqlDays
 						+ " "
 						+ sqlWhere
-						+ "ORDER BY transactionYear,transactionMonth,transactionDay,transactionID ASC");
+						+ "ORDER BY transactionYear,transactionMonth,transactionDay,transactionComment,transactionID ASC");
 		ps.setInt(1, showHidden);
 		ps.setString(2, toy);
 		ps.setString(3, foy);
